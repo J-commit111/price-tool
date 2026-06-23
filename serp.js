74380d1f6c2f50c38d8cc91820703b8c3b1f442c
J-commit111 <jay.mx111@gmail.com>
2026-06-23 16:52:15 +1000

    Add files via upload

---
+++ serp.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  try {
+    const params = new URLSearchParams(req.query);
+    const url = 'https://serpapi.com/search?' + params.toString();
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
