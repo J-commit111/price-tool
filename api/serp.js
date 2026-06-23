8dccf1438b4aeaee98d8c227d1a51d8f3c4a499a
J-commit111 <jay.mx111@gmail.com>
2026-06-23 17:02:11 +1000

    Update API to use new search endpoint and parameters

--- api/serp.js
+++ api/serp.js
@@ -3,8 +3,15 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   try {
-    const params = new URLSearchParams(req.query);
-    const url = 'https://serpapi.com/search?' + params.toString();
+    const { q, api_key, gl, hl } = req.query;
+    const params = new URLSearchParams({
+      engine: 'google_shopping',
+      q: q,
+      api_key: api_key,
+      gl: gl || 'au',
+      hl: hl || 'en'
+    });
+    const url = 'https://www.searchapi.io/api/v1/search?' + params.toString();
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
